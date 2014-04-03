2c232719a3b1f82ae8f869899def3b16b34a829d
kamuken <atoi0328@gmail.com>
2014-04-03 13:06:51 +0900

    Finish sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@ class User < ActiveRecord::Base
   before_save { email.downcase! }
 #  before_save { |user| user.email = email.downcase }
   
+  before_save :create_remember_token
+  
   validates :name, presence: true, length: { maximum: 50 }
   validates :password,length: {minimum: 6 }
   validates :password_confirmation, presence: true
@@ -23,4 +25,9 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
